c1ce987fd412cc9405a75c7e2b7fc10521416218
unknown <thalesjoazeiro@gmail.com>
2023-10-25 16:23:59 -0300

    Consumindo a Rick and Morty API

---
+++ script.js
@@ -0,0 +1,20 @@
+const characterId = document.getElementById('characterId');
+const btnGo = document.getElementById('btn-go');
+const content = document.getElementById('content');
+const image = document.getElementById('img');
+
+const fetchApi = (value) => {
+    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        return data;
+    });
+    return result;
+}
+
+fetchApi(1);
+
+btnGo.addEventListener('click', () => {
+
+})
